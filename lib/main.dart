3e612ffd2292351211aee8b355d061e54fe51f04
Nacho <nacho_085@hotmail.com>
2022-08-22 11:44:54 +0200

    Pantalla de edicion de producto creado

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ class MyApp extends StatelessWidget {
       routes: {
         'login': ( _ ) => LoginScreen(),
         'home': ( _ ) => HomeScreen(),
+        'product': ( _ ) => ProductScreen(),
+        
       },
       theme: ThemeData.light().copyWith(
         scaffoldBackgroundColor: Colors.grey[300],
