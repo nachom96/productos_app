9b7e2b5702b9d1f4ee83e68aa8eb81101cdb290b
Nacho <nacho_085@hotmail.com>
2022-08-26 15:59:41 +0200

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Productos App',
-      initialRoute: 'checking',
+      initialRoute: 'login',
       routes: {
 
         'checking': ( _ ) => CheckAuthScreen(),
